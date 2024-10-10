272374308f2ac2ff983344123ddd1499e19a6b6d
Lee Jiny <sintmtn69@gmail.com>
2024-10-10 19:00:18 +0900

    Title: 전화번호 바꾸기, Time: 12ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241010/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+char a,b,c,d,e,f,g,h,i,j,k,l,m;
+int main() {
+
+    cin >> a >> b >> c>>d>>e>>f>>g>>h>>i>>j>>k>>l>>m;
+
+    cout <<a<<b<<c<<d<<j<<k<<l<<m<<i<<e<<f<<g<<h<<'\n';
+    return 0;
+}
