5f1765cc7f6999ae5ca1a9d98abd9f213c5a2dc9
Lee Jiny <sintmtn69@gmail.com>
2024-10-10 17:35:44 +0900

    Title: 변수 출력하기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241010/변수 출력하기 3/outputing-variables-3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+char a,b,c;
+
+int main() {
+    a = '1';
+    b = '2';
+    c = 'C';
+
+    cout << a << "->" << b << "->" << c <<'\n';
+    return 0;
+}
