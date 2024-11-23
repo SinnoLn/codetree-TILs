304f2e5de9fcd89409ef20f6437590b9d68757e4
Lee Jiny <ok373737@naver.com>
2024-11-23 14:24:51 +0900

    Title: 9-1 정사각형, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241123/9-1 정사각형/9-1-square.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+int n,cnt;
+
+int main() {
+    cin >> n;
+    cnt = 9;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; j++){
+            cout << cnt <<" ";
+            cnt --;
+            if(cnt == 0)
+                cnt = 9;
+        }
+        cout << '\n';
+    }
+    return 0;
+}
