cc14084ea8660b17f84156a808204f8f808cd0db
Lee Jiny <ok373737@naver.com>
2024-11-25 15:02:50 +0900

    Title: 구구단으로 이루어진 직각 삼각형, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241125/구구단으로 이루어진 직각 삼각형/a-right-triangle-made-up-of-multiplication-tables.cpp
+++ 241125/구구단으로 이루어진 직각 삼각형/a-right-triangle-made-up-of-multiplication-tables.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
 using namespace std;
-int n;
+int n,finish;
 
 int main() {
     cin >> n;
-
+    finish = n;
     for(int i=1; i<=n; i++){
-        for(int j=1; j<n; j++){
+        for(int j=1; j<=n; j++){
             cout << i <<" * "<<j<< " = " <<i*j;
+            if(j == finish) break;
             cout << " / ";
         }
         cout << '\n';
+        finish--;
     }
     return 0;
 }
