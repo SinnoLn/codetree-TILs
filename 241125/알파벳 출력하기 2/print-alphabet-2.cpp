c36b97403a79f718b40f60c82e93c97025014dd3
Lee Jiny <ok373737@naver.com>
2024-11-25 15:20:04 +0900

    Title: 알파벳 출력하기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241125/알파벳 출력하기 2/print-alphabet-2.cpp
+++ 241125/알파벳 출력하기 2/print-alphabet-2.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
 using namespace std;
 
-int n;
+int n, up;
 int main() {
     cin >> n;
-
     for(int i=0; i<n; i++){
-        char c = 65 - i;
-        cout << c <<" ";
+        for(int j=0; j<i; j++){
+            cout <<"  ";
+        }
+
+        for(int j=n-i; j>0; j--){
+            char c = 65 + up;
+            cout << c <<" ";
+            up++;
+        }
+        cout << '\n';
     }
-    cout << '\n';
     return 0;
 }
