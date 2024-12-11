80394b421c38918e12b77684d78cd50fa475d1c0
Lee Jiny <ok373737@naver.com>
2024-12-11 15:34:12 +0900

    Title: 정수 명령 처리, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241211/정수 명령 처리/process-numeric-commands.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+int n, num;
+stack<int> st;
+int main() {
+    cin >> n;
+    for(int i=0; i<n; i++){
+        string s;
+        cin >> s;
+        if(s == "push"){
+            cin >> num;
+            st.push(num);
+        }
+        else if(s == "size"){
+            cout << st.size() <<'\n';
+        }
+        else if(s == "empty"){
+            cout << st.empty() <<'\n';
+        }
+        else if(s == "pop"){
+            cout << st.top() <<'\n';
+            st.pop();
+        }
+    }
+    return 0;
+}
