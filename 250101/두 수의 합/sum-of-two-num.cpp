904425dfaf294c451a7db358f99a68d8ecca4c08
Lee Jiny <ok373737@naver.com>
2025-01-01 12:39:17 +0900

    Title: 두 수의 합, Time: 40ms, Memory: 2MB, Status: Passed - CodeTree

--- 250101/두 수의 합/sum-of-two-num.cpp
+++ 250101/두 수의 합/sum-of-two-num.cpp
@@ -4,15 +4,15 @@ using namespace std;
 
 int n, k, num, answer;
 int main() {
-    unordered_map<int,int> hmap;
+    unordered_map<long,long> hmap;
     cin >> n >> k;
     for(int i=0; i<n; i++){
         cin >> num;
         //k가 합이되는 숫자가 있다면
         if(hmap.find(k-num) != hmap.end()){
-            answer++;
+            answer+= hmap[k-num];
         }
-        else hmap[num] = 1;
+        hmap[num]++;
     }
     cout << answer <<'\n';
     return 0;
