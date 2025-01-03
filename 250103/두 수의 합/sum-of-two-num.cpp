40bb6239fb7c90f824b39d8094d200a6900ad042
Lee Jiny <ok373737@naver.com>
2025-01-03 16:18:53 +0900

    Title: 두 수의 합, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250103/두 수의 합/sum-of-two-num.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+int n, k, num, answer;
+unordered_map<int,int> hashmap;
+
+int main() {
+    cin >> n >> k;
+    for(int i=0; i<n; i++){
+        cin >> num;
+        hashmap[num]++;
+        
+        if(hashmap.find(k-num) != hashmap.end()){
+            answer+=hashmap[num];
+        }
+    }
+    cout << answer <<'\n';
+    return 0;
+}
