04f40cd233211fdcb309f69ab465161f92752dcd
Lee Jiny <ok373737@naver.com>
2025-02-07 17:20:31 +0900

    Title: 2021년 날짜의 유무, Time: 135ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250207/2021년 날짜의 유무/with-or-without-2021.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int month = sc.nextInt();
+        int day = sc.nextInt();
+
+        if(isDate(month, day))System.out.println("Yes");
+        else System.out.println("No");
+        sc.close();
+    }
+
+    public static boolean isDate(int month, int day){
+        if(month == 2 && day < 29) return true;
+        if(month == 1 || month == 3||month == 5||month == 7||month == 8||month == 10||month == 12){
+            if(day<32) return true;
+        }
+        else {
+            if(day<31) return true;
+        }
+        return false;
+    }
+}
