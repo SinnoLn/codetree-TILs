c4e634960fddc59ffc468bbfbcab56996150fe78
Lee Jiny <ok373737@naver.com>
2025-02-07 17:25:29 +0900

    Title: 2021년 날짜의 유무, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

--- 250207/2021년 날짜의 유무/with-or-without-2021.java
+++ 250207/2021년 날짜의 유무/with-or-without-2021.java
@@ -19,7 +19,7 @@ public class Main {
         if(month == 1 || month == 3||month == 5||month == 7||month == 8||month == 10||month == 12){
             if(day<32) return true;
         }
-        else {
+        else if(month == 4 || month == 6||month == 9||month == 7||month == 11) {
             if(day<31) return true;
         }
         return false;
