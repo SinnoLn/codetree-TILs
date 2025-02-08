4645111e4a24d2a178e0e87df6b7cd08c526c768
Lee Jiny <ok373737@naver.com>
2025-02-08 11:39:23 +0900

    Title: 두 정수에 대한 연산값 2, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250208/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] a = new int[1];
+        int[] b = new int[1];
+        a[0] = sc.nextInt();
+        b[0] = sc.nextInt();
+
+        cal(a,b);
+        System.out.println(a[0] + " "+ b[0]);
+        sc.close();
+    }
+    public static void cal(int[] a, int[] b){
+        if(a[0]>b[0]){
+            a[0]*=2;
+            b[0]+=10;
+        }
+        else{
+            a[0]+=10;
+            b[0]*=2;
+        }
+    }
+}
