9e5500c863ea9d58dd5f89afbf7028502ce4cec4
Lee Jiny <ok373737@naver.com>
2025-02-10 13:28:18 +0900

    Title: 왔다 갔던 구역 2, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

--- 250210/왔다 갔던 구역 2/area-been-to-and-from2.java
+++ 250210/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -3,38 +3,34 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] arr = new int[2000];
+        
+        int[] a = new int[2000];
+        int n = sc.nextInt(); 
+        
+        int position = 1000;
 
-        int n = sc.nextInt();
-        int point = 1000;
-
-        for(int i=0; i<n; i++){
+        for (int i = 0; i < n; i++) {
             int num = sc.nextInt();
             char dir = sc.next().charAt(0);
-            
-            if(dir == 'R'){
-                while(num>0){
-                    arr[point]++;
-                    point++;
-                    num--;
+
+            while (num > 0) {
+                if (dir == 'R') { 
+                    a[position]++;
+                    position++; 
+                } 
+                else { 
+                    position--; 
+                    a[position]++;
                 }
-            }
-            else{
-                while(num>0){
-                    arr[point]++;
-                    point--;
-                    num--;
-                }
+                num--;
             }
         }
 
         int cnt = 0;
-        for(int i=0; i<arr.length; i++){
-            if(arr[i]>1){
-               // System.out.println("arr["+i+"] : " + arr[i]);
-                cnt++;
-            } 
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] > 1) cnt++;
         }
+
         System.out.println(cnt);
         sc.close();
     }
