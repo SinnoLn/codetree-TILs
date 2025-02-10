ffbe50cae224ba4175db9c76c632be3e8910cc6b
Lee Jiny <ok373737@naver.com>
2025-02-10 17:35:04 +0900

    Title: 연속되는 수 2, Time: 197ms, Memory: 12MB, Status: Passed - CodeTree

--- 250210/연속되는 수 2/continuous-number2.java
+++ 250210/연속되는 수 2/continuous-number2.java
@@ -5,12 +5,18 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr = new int[n];
-        int cnt = 0;
+        int cnt = 1;
+        int maxVal = 0;
         for(int i=0 ; i<n; i++){
             arr[i] = sc.nextInt();
-            if(i==0 || arr[i] != arr[i-1]) cnt++;
+            if(i == 0 || arr[i] != arr[i - 1]){
+                if(maxVal<cnt) maxVal = cnt;
+                cnt = 1;
+            }
+            else cnt++;
         }
-        System.out.println(cnt);
+        if(maxVal<cnt) maxVal = cnt;
+        System.out.println(maxVal);
         sc.close();
     }
 }
