2038a493a725efe85c49b941f4e5e8d9ea624452
Lee Jiny <ok373737@naver.com>
2025-02-18 14:29:24 +0900

    Title: 선두를 지켜라, Time: 202ms, Memory: 28MB, Status: Passed - CodeTree

--- 250218/선두를 지켜라/keep-the-lead.java
+++ 250218/선두를 지켜라/keep-the-lead.java
@@ -10,36 +10,44 @@ public class Main {
         int b[] = new int[1000000];
 
         int time = 0;
+        int dis = 0;
         for(int i=0; i<n; i++){
             int v = sc.nextInt();
             int t = sc.nextInt();
 
             while(t>0){
                 time++;
-                a[time]=v;
+                dis+=v;
+                a[time]=dis;
                 t--; 
             }
         }
 
         time = 0;
+        dis = 0;
         for(int i=0; i<m; i++){
             int v = sc.nextInt();
             int t = sc.nextInt();
 
             while(t>0){
                 time++;
-                b[time]=v;
+                dis+=v;
+                b[time]=dis;
                 t--; 
             }
         }
 
         int cnt = 0;
-        for(int i=2; i<time; i++){
-            if((b[i]>a[i] && b[i-1]<a[i-1]) || (b[i]<a[i] && b[i-1]>a[i-1])){
-                cnt ++;
-            }
+        int preLeader = 0;
+        int leader = 0;
+        for(int i=1; i<time; i++){
+            if(a[i]<b[i]) leader = 2;
+            else if(a[i]>b[i]) leader = 1;
+
+            if(leader != preLeader) cnt++;
+            preLeader = leader;
         }
-        System.out.println(cnt);
+        System.out.println(cnt-1);
         sc.close();
     }
 }
