736b45058ac4a819a7aa79b66e80d9038cdf2c61
Lee Jiny <ok373737@naver.com>
2025-02-18 15:07:08 +0900

    Title: 좌우로 움직이는 로봇, Time: 869ms, Memory: 41MB, Status: Runtime Error - CodeTree

---
+++ 250218/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
@@ -0,0 +1,74 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int a[] = new int[1000000];
+        int b[] = new int[1000000];
+
+        int timeA = 0;
+        int posA = 0;
+        for(int i=0; i<n; i++){
+            int num = sc.nextInt();
+            char dir = sc.next().charAt(0);
+
+            if(dir == 'R'){
+                while(num>0){
+                    timeA++;
+                    posA++;
+                    a[timeA] = posA; 
+                    num--;
+                }
+            }
+            else if (dir == 'L'){
+                while(num>0){
+                    timeA++;
+                    posA--;
+                    a[timeA] = posA; 
+                    num--;
+                }
+            }
+        }
+
+        int timeB = 0;
+        int posB = 0;
+        for(int i=0; i<m; i++){
+            int num = sc.nextInt();
+            char dir = sc.next().charAt(0);
+
+            if(dir == 'R'){
+                while(num>0){
+                    timeB++;
+                    posB++;
+                    b[timeB] = posB; 
+                    num--;
+                }
+            }
+            else if (dir == 'L'){
+                while(num>0){
+                    timeB++;
+                    posB--;
+                    b[timeB] = posB; 
+                    num--;
+                }
+            }
+        }
+        
+        int maxTime = Math.max(timeA, timeB);
+        for (int i = 1; i <= maxTime; i++) {
+            if (i > timeA) a[i] = a[i - 1];
+            if (i > timeB) b[i] = b[i - 1];
+        }
+
+        int cnt = 0;
+        for(int i=2; i<maxTime; i++){
+            if(a[i]==b[i] && a[i-1]!=b[i-1]) cnt++; 
+        }
+        System.out.println(cnt);
+        sc.close();
+    }
+}
