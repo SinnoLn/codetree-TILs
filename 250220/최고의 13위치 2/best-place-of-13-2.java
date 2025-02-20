0df6e6bf5199a2ccc9a46b7c2edc87caaab408fe
Lee Jiny <ok373737@naver.com>
2025-02-20 20:51:33 +0900

    Title: 최고의 13위치 2, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

--- 250220/최고의 13위치 2/best-place-of-13-2.java
+++ 250220/최고의 13위치 2/best-place-of-13-2.java
@@ -14,8 +14,9 @@ public class Main {
         int ans = 0;
         for(int i=0; i<n; i++){
             for(int j=0; j<n-2; j++){
-                for(int k=i+1; k<n; k++){
+                for(int k=0; k<n; k++){
                     for(int m=0; m<n-2; m++){
+                        if(i==k && (j<=m+2 && m<=j+2)) continue;
                         int tmp = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[k][m] + arr[k][m+1] + arr[k][m+2];
                         ans = Math.max(tmp,ans);        
                     }
