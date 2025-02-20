d977381dd3956cbaa0305734909eebe3250effdf
Lee Jiny <ok373737@naver.com>
2025-02-20 16:39:10 +0900

    Title: Carry 피하기 2, Time: 147ms, Memory: 11MB, Status: Wrong Answer - CodeTree

--- 250220/Carry 피하기 2/escaping-carry-2.java
+++ 250220/Carry 피하기 2/escaping-carry-2.java
@@ -13,11 +13,13 @@ public class Main {
             for(int j=i+1; j<n-1; j++){
                 for(int k=j+1; k<n; k++){
                     int len = Math.min(Integer.toString(arr[i]).length(),Math.min(Integer.toString(arr[j]).length(),Integer.toString(arr[k]).length()));
+                    int mlen = Math.max(Integer.toString(arr[i]).length(),Math.max(Integer.toString(arr[j]).length(),Integer.toString(arr[k]).length()));
                     String s1 = new StringBuilder(Integer.toString(arr[i])).reverse().toString();
                     String s2 = new StringBuilder(Integer.toString(arr[j])).reverse().toString();
                     String s3 = new StringBuilder(Integer.toString(arr[k])).reverse().toString();
                     int sum = arr[i] + arr[j] + arr[k];
                     boolean check = true;
+                    if(Integer.toString(sum).length()>mlen) check = false;
                     for(int t=0; t<len; t++){
                         if(s1.charAt(t)-'0'+s2.charAt(t)-'0'+s3.charAt(t)-'0' > 9){
                             check = false;
