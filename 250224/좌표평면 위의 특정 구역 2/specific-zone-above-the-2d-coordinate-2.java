798aee0e4b82e4a1fac85184ba16e28ca5a14b7b
Lee Jiny <ok373737@naver.com>
2025-02-24 13:30:17 +0900

    Title: 좌표평면 위의 특정 구역 2, Time: 132ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250224/좌표평면 위의 특정 구역 2/specific-zone-above-the-2d-coordinate-2.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[][] arr = new int[n][2];
+
+        for(int i=0; i<n; i++){
+            arr[i][0] = sc.nextInt();
+            arr[i][1] = sc.nextInt();
+        }
+
+        int ans = Integer.MAX_VALUE;
+
+        for(int i=0; i<n; i++){
+            int max_h = Integer.MIN_VALUE;
+            int min_h = Integer.MAX_VALUE;
+            int max_w = Integer.MIN_VALUE;
+            int min_w = Integer.MAX_VALUE;
+            for(int j=0; j<n; j++){
+                if(i == j) continue;
+                max_h = Math.max(max_h, arr[j][0]);
+                min_h = Math.min(min_h, arr[j][0]);
+                max_w = Math.max(max_w, arr[j][1]);
+                min_w = Math.min(min_w, arr[j][1]);
+            }
+            if(min_h == max_h || min_w == max_w) continue;
+            ans = Math.min(ans, (max_h-min_h)*(max_w-min_w));
+        }
+        System.out.println(ans);
+        sc.close();
+    }
+}
