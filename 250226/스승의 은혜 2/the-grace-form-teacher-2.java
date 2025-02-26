7c0585b4fe34ed3888591e34f528e2972c6bd5b9
Lee Jiny <ok373737@naver.com>
2025-02-26 14:48:11 +0900

    Title: 스승의 은혜 2, Time: 164ms, Memory: 11MB, Status: Passed - CodeTree

--- 250226/스승의 은혜 2/the-grace-form-teacher-2.java
+++ 250226/스승의 은혜 2/the-grace-form-teacher-2.java
@@ -8,20 +8,21 @@ public class Main {
 
         int[] gift = new int[n];
         for(int i=0; i<n; i++) gift[i] = sc.nextInt();
-        Arrays.sort(gift);
 
+        Arrays.sort(gift);
         //그냥 완탐으로 품
         int maxNum = 0;
         for(int i=0; i<n; i++){
             int tmp = 0;
             int count = 0;
+            gift[i]/=2;
             for(int j=0; j<n; j++){
-                if(tmp>maxBuget) break;
-                if(i == j) tmp+=gift[j]/2;
+                if(tmp + gift[j] > maxBuget) break;
                 tmp += gift[j];
                 count++;
             }
-            maxNum = Math.max(maxNum, count);
+            gift[i]*=2;
+            maxNum = Math.max(count, maxNum);
         }
         System.out.println(maxNum);
         sc.close();
