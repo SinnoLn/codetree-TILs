fec518652afb706ed67beca3ccbeb697f371eddb
Lee Jiny <ok373737@naver.com>
2025-03-01 22:25:50 +0900

    Title: 최소 와이파이 수, Time: 151ms, Memory: 10MB, Status: Passed - CodeTree

--- 250301/최소 와이파이 수/minimum-number-of-wifi.java
+++ 250301/최소 와이파이 수/minimum-number-of-wifi.java
@@ -4,31 +4,21 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int m = sc.nextInt();  
+        int m = sc.nextInt();
+
         int[] arr = new int[n];
+        for(int i=0; i<n; i++) arr[i] = sc.nextInt();
 
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
+        int wifi = 0;
 
-        int wifiCount = 0; 
-        int position = 0;  
-
-        while (position < n) {
-        
-            int installPos = position + m;
-            if (installPos >= n) installPos = n - 1;  
-
-            while (installPos > position && arr[installPos] == 0) {
-                installPos--;
+        for(int i=0; i<n; i++){
+            if(arr[i]==1){
+                wifi++;
+                i+=2*m;
             }
-
-            if (installPos != position) wifiCount++;
-
-            position = installPos + m + 1;  
-        }
-
-        System.out.println(wifiCount);
+        };
+        
+        System.out.println(wifi);
         sc.close();
     }
 }
