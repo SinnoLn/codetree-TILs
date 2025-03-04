a15c39e93876ac3c41dab61653d1c206e7748ad4
Lee Jiny <ok373737@naver.com>
2025-03-04 09:59:23 +0900

    Title: 갈 수 있는 곳들, Time: 195ms, Memory: 13MB, Status: Passed - CodeTree

--- 250304/갈 수 있는 곳들/places-can-go.java
+++ 250304/갈 수 있는 곳들/places-can-go.java
@@ -13,17 +13,16 @@ public class Main {
             for(int j=0; j<n; j++) arr[i][j] = sc.nextInt();
         }
 
-        int dy[] = {0,0,-1,1,-1,-1,1,1};
-        int dx[] = {-1,1,0,0,-1,1,-1,1};
+        int dy[] = {0,0,-1,1};
+        int dx[] = {-1,1,0,0};
+        Queue<int[]> q = new LinkedList<>();
 
         for(int i=0; i<m; i++){
             int y = sc.nextInt()-1;
             int x = sc.nextInt()-1;
-
-            Queue<int[]> q = new LinkedList<>();
     
             if(visited[y][x]) continue;
-            
+
             q.add(new int[]{y,x});
             visited[y][x] = true;
             ans++;
@@ -33,7 +32,7 @@ public class Main {
                 int currY = curr[0];
                 int currX = curr[1];
 
-                for(int j=0; j<8; j++){
+                for(int j=0; j<4; j++){
                     int ny = currY + dy[j];
                     int nx = currX + dx[j];
 
