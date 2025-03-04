26551de5629128b3aeea637d22d74c6678e1b83b
Lee Jiny <ok373737@naver.com>
2025-03-04 09:09:50 +0900

    Title: 갈 수 있는 곳들, Time: 253ms, Memory: 18MB, Status: Wrong Answer - CodeTree

---
+++ 250304/갈 수 있는 곳들/places-can-go.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] arr = new int[n][n];
+        boolean[][] visited = new boolean[n][n];
+        int ans = 0;
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++) arr[i][j] = sc.nextInt();
+        }
+
+        int dy[] = {0,0,-1,1,-1,-1,1,1};
+        int dx[] = {-1,1,0,0,-1,1,-1,1};
+
+        for(int i=0; i<m; i++){
+            int y = sc.nextInt()-1;
+            int x = sc.nextInt()-1;
+
+            Queue<int[]> q = new LinkedList<>();
+    
+            if(visited[y][x]) continue;
+            
+            q.add(new int[]{y,x});
+            visited[y][x] = true;
+            ans++;
+            
+            while(!q.isEmpty()){
+                int[] curr = q.poll();
+                int currY = curr[0];
+                int currX = curr[1];
+
+                for(int j=0; j<8; j++){
+                    int ny = currY + dy[j];
+                    int nx = currX + dx[j];
+
+                    if(ny < 0 || nx < 0 || ny >= n || nx >= n) continue;
+                    if(visited[ny][nx] == true || arr[ny][nx] == 1) continue;
+                    visited[ny][nx] = true;
+                    ans++;
+                    q.add(new int[] {ny, nx});
+                }
+            }
+        }
+        System.out.println(ans);
+        sc.close();
+    }
+}
