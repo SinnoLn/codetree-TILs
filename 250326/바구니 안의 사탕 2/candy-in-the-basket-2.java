a170c8465a00f6e883d634a4d41ded566694cc50
Lee Jiny <ok373737@naver.com>
2025-03-26 11:08:17 +0900

    Title: 바구니 안의 사탕 2, Time: 95ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250326/바구니 안의 사탕 2/candy-in-the-basket-2.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.io.*;
+
+//같은 위치에 여러 바구니
+//최대 100개의 바구니
+//바구니 위치는 0부터 100
+//k가 조금 애매함
+//사탕의 개수 바구니의 좌표
+//누가봐도 완전탐색
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+
+        int n = Integer.parseInt(input[0]);
+        int k = Integer.parseInt(input[1]);
+
+        int[] candy = new int[101];
+        int sum = 0;
+        for(int i=0; i<n; i++){
+            input = br.readLine().split(" ");
+            int num = Integer.parseInt(input[0]);
+            int position = Integer.parseInt(input[1]);
+
+            candy[position]+=num;
+            sum+=num;
+        }
+
+
+        int ans = 0;
+        if(k >= 50){
+            System.out.println(sum);
+            return;
+        }
+        for(int c = k; c<101; c++){
+            int tmp = 0;
+            for(int j=c-k; j<c+k+1; j++){
+                if(j > 100) break;
+                tmp+=candy[j];
+            }
+            ans = Math.max(tmp, ans);
+        }
+        System.out.println(ans);
+    }
+}
