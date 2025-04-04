f8961db9a363540de62b1995ac7e4ef4a9418382
Lee Jiny <ok373737@naver.com>
2025-04-04 20:41:35 +0900

    Title: 바이러스 검사, Time: 1390ms, Memory: 112MB, Status: Passed - Codetree

--- 250404/바이러스 검사/virus-detector.java
+++ 250404/바이러스 검사/virus-detector.java
@@ -1,22 +1,23 @@
 import java.io.*;
 import java.util.*;
 
+//팀장 1
+//팀원 0 ~ 이상
+//식당의 수 n 1,000,000
+//각 식당에 있는 고객 수 1,000,000
+//검사팀장 검사가능한 최대 고객수, 검사팀원이 검사 가능한 최대 고객수  1,000,000
 public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] people = new int[n];
-        for(int i=0; i<n; i++) people[i] = Integer.parseInt(st.nextToken());
-        st = new StringTokenizer(br.readLine());
-        int[] check = new int[2];
-        for(int i=0; i<2; i++) check[i] = Integer.parseInt(st.nextToken());
+        String[] people = br.readLine().split(" ");
+        String[] check = br.readLine().split(" ");
         long ans = 0;
 
-        int lc = check[0];
-        int ec = check[1];
+        int lc = Integer.parseInt(check[0]);
+        int ec = Integer.parseInt(check[1]);
         for(int i=0; i<n; i++){
-            int p = people[i];
+            int p = Integer.parseInt(people[i]);
             p-=lc;
             if(p>0){
                 ans+=(p/ec);
