a66214772d25ecd0e549bb52336ec80f5da2077c
Lee Jiny <ok373737@naver.com>
2025-04-11 19:16:50 +0900

    Title: 트로미노, Time: 124ms, Memory: 10MB, Status: Passed - Codetree

--- 250411/트로미노/tromino.java
+++ 250411/트로미노/tromino.java
@@ -3,11 +3,11 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(Systm.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt();
-        int m = Integer.parseInt();
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
         int[][] arr = new int[n][m];
         for(int i=0; i<n; i++){
@@ -19,18 +19,34 @@ public class Main {
 
         //먼저 기역 블록 계산
         int ans = 0;
+        for (int i = 0; (i + 1) < n; i++) {
+            for (int j = 0; (j + 1) < m; j++) {
+                int sum = arr[i][j] + arr[i][j+1] + arr[i+1][j] + arr[i+1][j+1];
+
+                // 기역자 블록 (2x2에서 한 칸 제거) → 4가지
+                ans = Math.max(ans, sum - arr[i][j]);
+                ans = Math.max(ans, sum - arr[i][j+1]); 
+                ans = Math.max(ans, sum - arr[i+1][j]);
+                ans = Math.max(ans, sum - arr[i+1][j+1]);
+            }       
+        }
+
+
+        //일자 블록도 계산
+        //행으로 그리고 열로
         for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                for(int y = i; y<i+2; y++){
-                    int sum = 0;
-                    for(int x=j; x<j+2; x++){
-                        sum+=arr[y][x];
-                    }
-                    ans = Math.max(ans, sum-arr[y][x]);
-                    ans = Math.max(ans, sum-arr[y][x]);
-                    ans = Math.max(ans, sum-arr[y][x]);
-                }
+            for(int j=0; (j+2)<m; j++){
+                int sum = arr[i][j] + arr[i][j+1] + arr[i][j+2];
+                ans = Math.max(ans, sum);
             }
         }
+
+        for(int i=0; i<m; i++){
+            for(int j=0; (j+2)<n; j++){
+                int sum = arr[j][i] + arr[j+1][i] + arr[j+2][i];
+                ans = Math.max(ans, sum);
+            }
+        }
+        System.out.println(ans);
     }
 }
