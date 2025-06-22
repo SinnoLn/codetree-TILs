0ce1b853ec1f7567c99519bbfcd5f19ae5aea5ae
Lee Jiny <ok373737@naver.com>
2025-06-22 15:21:30 +0900

    Title: 배열의 평균, Time: 100ms, Memory: 9MB, Status: Passed - Codetree

--- 250622/배열의 평균/ave-of-array.java
+++ 250622/배열의 평균/ave-of-array.java
@@ -6,24 +6,27 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int[][] list = new int[2][4];
         double allSum = 0.0;
+
         for(int i=0; i<2; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
             double sum = 0.0;
             for(int j=0; j<4; j++){
                 list[i][j] = Integer.parseInt(st.nextToken());
                 sum+=list[i][j];
+                allSum+=list[i][j];
             }
             System.out.printf("%.1f ",sum/4);
         }
         System.out.println();
 
-        for(int i=0; i<2; i++){
+        for(int i=0; i<4; i++){
             double sum = 0.0;
-            for(int j=0; j<4; j++){
-                sum+=list[i][j];
+            for(int j=0; j<2; j++){
+                sum+=list[j][i];
             }
             System.out.printf("%.1f ",sum/2);
         }
         System.out.println();
+        System.out.printf("%.1f",allSum/8);
     }
 }
