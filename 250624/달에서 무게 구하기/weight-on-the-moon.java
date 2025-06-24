c0075405fab9a0a8e1b75d8e1623ff24d369995e
Lee Jiny <ok373737@naver.com>
2025-06-24 19:44:51 +0900

    Title: 달에서 무게 구하기, Time: 104ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250624/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        int weigth = 13;
+        double ratio = 0.165;
+
+        System.out.printf("%d * %.6f = %.6f",weigth,ratio,weigth*ratio);
+    }
+}
