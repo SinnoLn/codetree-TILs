d50077bfce854b66ae1a667daa8bfcbb1d3b1da0
Lee Jiny <ok373737@naver.com>
2025-06-25 20:11:35 +0900

    Title: 두 실수의 곱, Time: 97ms, Memory: 9MB, Status: Passed - Codetree

--- 250625/두 실수의 곱/the-product-of-two-real-numbers.java
+++ 250625/두 실수의 곱/the-product-of-two-real-numbers.java
@@ -8,15 +8,3 @@ public class Main {
         System.out.printf("%.3f", a*b);
     }
 }
-import java.util.*;
-
-public class Main {
-    public static void main(String[] args) {
-        double a = 5.26;
-        double b = 8.27;
-
-        System.out.printf("%.3f", a*b);
-    \}
-\}
-
-$0
