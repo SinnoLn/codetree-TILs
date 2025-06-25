61fed81684f191f2e80f3bc3949ad7410ce11444
Lee Jiny <ok373737@naver.com>
2025-06-25 20:26:19 +0900

    Title: 정수 복사, Time: 89ms, Memory: 9MB, Status: Passed - Codetree

--- 250625/정수 복사/copy-integer.java
+++ 250625/정수 복사/copy-integer.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        int a = 2;
-        int b = 6;
+        int a = 3;
+        int b = 4;
 
-        a = b;
-        System.out.println(a);
-        System.out.println(b);
+        b = a;
+        System.out.println(a +" "+b);
+        System.out.println(a*b);
     }
 }
